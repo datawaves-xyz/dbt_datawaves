5dfa22c65b35d2edce940f5f2ac8f2e0c3a938e0
onesuper <onesuperclark@gmail.com>
2022-04-10 18:02:25 +0800

    Start using dbt macro packages

--- models/nft/wyvern_data.sql
+++ models/nft/wyvern_data.sql
@@ -25,8 +25,10 @@ select
     else addrs[6]
   end as currency_token,
   case
-    when substring(calldatabuy, 1, 4) in ({{ binary_literal('fb16a595') }}, {{ binary_literal('96809f90') }}) then cast(conv(hex(substring(calldatabuy, 101, 32)), 16, 10) as string)
-    when substring(calldatabuy, 1, 4) in ({{ binary_literal('23b872dd') }}, {{ binary_literal('f242432a') }}) then cast(conv(hex(substring(calldatabuy, 69, 32)), 16, 10) as string)
+    when substring(calldatabuy, 1, 4) in ({{ binary_literal('fb16a595') }}, {{ binary_literal('96809f90') }})
+      then cast(conv(hex(substring(calldatabuy, 101, 32)), 16, 10) as {{ dbt_utils.type_string() }})
+    when substring(calldatabuy, 1, 4) in ({{ binary_literal('23b872dd') }}, {{ binary_literal('f242432a') }})
+      then cast(conv(hex(substring(calldatabuy, 69, 32)), 16, 10) as {{ dbt_utils.type_string() }})
   end as token_id
 
 from {{ var('wyvern_atomic_match') }}
