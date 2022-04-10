f1e74e24310a4fd84ec58ca30b61979d10325464
onesuper <onesuperclark@gmail.com>
2022-04-10 22:29:53 +0800

    Add macro: displayed_amount

---
+++ macros/utils/displayed_amount.sql
@@ -0,0 +1,22 @@
+{% macro displayed_amount(orginal_amount, decimals) %}
+  {{ return(adapter.dispatch('displayed_amount', 'ethereum') (orginal_amount, decimals)) }}
+{% endmacro %}
+
+
+{% macro default__displayed_amount(orginal_amount, decimals) -%}
+
+    {{ orginal_amount }} / power(10, {{ decimals }})
+
+{%- endmacro %}
+
+{% macro postgres__displayed_amount(orginal_amount, decimals) -%}
+
+    {{ orginal_amount }} / 10 ^ {{ decimals }}
+
+{%- endmacro %}
+
+{% macro spark__displayed_amount(orginal_amount, decimals) -%}
+
+    {{ orginal_amount }} / power(10, {{ decimals }})
+
+{%- endmacro %}
