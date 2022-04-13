2d4f9e286a3b10bd2a9e4534a678ca48030ed801
onesuper <onesuperclark@gmail.com>
2022-04-13 18:57:08 +0800

    Optimize token transfer erc721

--- models/erc721/erc721_token_transfers.sql
+++ models/erc721/erc721_token_transfers.sql
@@ -11,19 +11,23 @@ token_transfers as (
 erc721 as (
   select *
   from contracts where is_erc721
+),
+
+renamed as (
+  select
+    token_transfers.token_address,
+    token_transfers.from_address,
+    token_transfers.to_address,
+    token_transfers.value as token_id,
+    token_transfers.transaction_hash,
+    token_transfers.log_index,
+    token_transfers.block_timestamp,
+    token_transfers.block_number,
+    token_transfers.block_hash,
+    token_transfers.dt
+  from token_transfers
+
+  inner join erc721 on erc721.address = token_transfers.token_address
 )
 
-select
-  token_transfers.token_address,
-  token_transfers.from_address,
-  token_transfers.to_address,
-  token_transfers.value as token_id,
-  token_transfers.transaction_hash,
-  token_transfers.log_index,
-  token_transfers.block_timestamp,
-  token_transfers.block_number,
-  token_transfers.block_hash,
-  token_transfers.dt
-from token_transfers
-
-inner join erc721 on erc721.address = token_transfers.token_address
+select * from renamed
