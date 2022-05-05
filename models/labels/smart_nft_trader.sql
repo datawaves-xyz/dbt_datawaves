ac8251933281d6cf80df0ae4a611546bed50064e
onesuper <onesuperclark@gmail.com>
2022-05-05 18:55:06 +0800

    Fix trader.address and reformat sql

--- models/labels/smart_nft_trader.sql
+++ models/labels/smart_nft_trader.sql
@@ -9,33 +9,39 @@ contracts as (
 ),
 
 trader as (
-  select 
-    address, 
+  select
+    address,
     sum(amount) as profit
   from (
-    select seller as address, currency_amount as amount
-    from ethereum_nft.nft_trades
-    where currency_symbol in ('ETH','WETH')
+    select
+      seller as address,
+      currency_amount as amount
+    from nft_trades
+    where currency_symbol in ('ETH', 'WETH')
 
     union all
-    select buyer as address, -currency_amount as amount
-    from ethereum_nft.nft_trades
-    where currency_symbol in ('ETH','WETH')
+    select
+      buyer as address,
+      -currency_amount as amount
+    from nft_trades
+    where currency_symbol in ('ETH', 'WETH')
   )
-  group by 1
+  group by address
 ),
 
 smart_trader as (
-  select trader.address, trader.profit
+  select
+    trader.address,
+    trader.profit
   from trader
-  left anti join contracts 
-  on stat.address = contracts.address
+  left anti join contracts
+    on trader.address = contracts.address
   order by trader.profit desc
   limit 100
 )
 
-select 
-  address, 
+select
+  address,
   'Smart NFT Trader' as label,
   'Smart Money' as label_type
 from smart_trader
