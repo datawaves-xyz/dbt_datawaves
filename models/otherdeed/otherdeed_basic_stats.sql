94d093c14a9afaeb553b2dae133e29ff7fcadc55
onesuper <onesuperclark@gmail.com>
2022-05-18 18:14:40 +0800

    add schedule for otherdeed; optimize sql

--- models/otherdeed/otherdeed_basic_stats.sql
+++ models/otherdeed/otherdeed_basic_stats.sql
@@ -1,9 +1,16 @@
-with trade_info as (
+{{
+  cte_import([
+    ('otherdeed', 'otherdeed'),
+    ('nft_trades', 'nft_trades')
+  ])
+}},
+
+trade_info as (
   select
     nft_token_id,
     avg(eth_amount) as avg_eth_amount,
     max(eth_amount) as max_eth_amount
-  from ethereum_nft.nft_trades
+  from nft_trades
   where nft_contract_address = '0x34d85c9cdeb23fa97cb08333b511ac86e1c4e258'
   group by nft_token_id
 ),
@@ -14,7 +21,7 @@ token_info as (
     o.has_koda,
     t.avg_eth_amount,
     t.max_eth_amount
-  from ethereum_nft_metadata.otherdeed o
+  from otherdeed o
   left outer join trade_info t
     on t.nft_token_id = o.token_id
 )
