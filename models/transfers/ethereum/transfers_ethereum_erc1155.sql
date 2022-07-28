51e1f68893af98f81c244909520daf60fec459c9
onesuper <onesuperclark@gmail.com>
2022-07-28 15:26:20 +0800

    Fix sql

--- models/transfers/ethereum/transfers_ethereum_erc1155.sql
+++ models/transfers/ethereum/transfers_ethereum_erc1155.sql
@@ -1,23 +1,23 @@
 with erc1155_ids_batch as (
   select
-    *
-    explode(ids) as exploded_id,
+    *,
+    explode(ids) as exploded_id
   from {{ source('ethereum_common', 'erc_1155_evt_transfer_batch') }} 
 ),
 
 erc1155_values_batch as (
-    select
-    *
-    explode(values) as exploded_value,
+  select
+    *,
+    explode(values) as exploded_value
   from {{ source('ethereum_common', 'erc_1155_evt_transfer_batch') }} 
-)
+),
 
 erc1155_transfers_batch as (
   select
     distinct erc1155_ids_batch.exploded_id,
     erc1155_values_batch.exploded_value,
     erc1155_ids_batch.to,
-    erc1155_ids_batch.from.
+    erc1155_ids_batch.from,
     erc1155_ids_batch.contract_address,
     erc1155_ids_batch.evt_block_time
   from erc1155_ids_batch
@@ -29,7 +29,7 @@ erc1155_transfers_batch as (
 erc1155_transfers_single as (
   select *
   from {{ source('ethereum_common', 'erc_1155_evt_transfer_single') }}
-)
+),
 
 received_transfers as (
   select
@@ -49,7 +49,7 @@ received_transfers as (
     exploded_id as token_id,
     exploded_value as amount
   from erc1155_transfers_batch
-) 
+),
 
 sent_transfers as (
   select
